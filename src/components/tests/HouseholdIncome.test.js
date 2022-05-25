7456ff50211003e1bf9c2d21f3853a116b5f8813
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-05-25 11:47:21 +0200

    Added Second Test

--- src/components/tests/HouseholdIncome.test.js
+++ src/components/tests/HouseholdIncome.test.js
@@ -1,5 +1,6 @@
 import HouseholdIncome from "../UI/HouseholdIncome";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
 describe("Testing our form interaction...", () => {
 
@@ -11,5 +12,18 @@ describe("Testing our form interaction...", () => {
 
         expect(fullNameInput.value).toBe(""); //checking to see if the input is empty
         expect(amountInput.value).toBe(""); //checking to see if the input is empty
+    })
+
+    test("checking to see if the value we type gets read...", () => {
+        render(<HouseholdIncome/>);
+
+        const fullNameInput = screen.getByLabelText(/names/i);
+        const amountInput = screen.getByLabelText(/income/i);
+
+        userEvent.type(fullNameInput, "Wiaan Duvenhage");
+        userEvent.type(amountInput, "3453");
+
+        expect(fullNameInput.value).toBe("Wiaan Duvenhage");
+        expect(amountInput.value).toBe("3453");
     })
 })
